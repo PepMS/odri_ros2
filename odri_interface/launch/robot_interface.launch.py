eb42f1f9818d49d68988e8e05f4137d2c10f3d6d
PepMS <jmarti@iri.upc.edu>
2022-11-30 14:56:00 +0100

    style: rename launch files

---
+++ odri_interface/launch/robot_interface.launch.py
@@ -0,0 +1,19 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    config = os.path.join(get_package_share_directory('odri_interface'), 'config', 'params.yaml')
+
+    node = Node(package='odri_interface',
+                name='robot_interface',
+                executable='robot_interface',
+                output='screen',
+                emulate_tty=True,
+                parameters=[config])
+    ld.add_action(node)
+    
+    return ld
