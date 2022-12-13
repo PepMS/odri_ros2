68a753f6a3486152224ea144e06e2a8d5eba5cb4
PepMS <jmarti@iri.upc.edu>
2022-12-13 11:47:32 +0100

    refactor: organize launch files

--- odri_interface/launch/_robot_interface.launch.py
+++ odri_interface/launch/_robot_interface.launch.py
@@ -2,28 +2,16 @@ from launch_ros.actions import Node
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
-
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
     robot_name_arg = DeclareLaunchArgument('robot_name',
-                                           default_value='flying_arm_2',
                                            description='Name of the robot controlled by the ODRI master board.')
-    ld.add_action(robot_name_arg)
-
-    default_yaml_path = PathJoinSubstitution([
-        FindPackageShare('odri_interface'), 'config',
-        PythonExpression(["'", LaunchConfiguration('robot_name'), "'+ '.yaml'"])
-    ])
-
     yaml_path_arg = DeclareLaunchArgument('yaml_path',
-                                          default_value=default_yaml_path,
                                           description='Path of the yaml file with state publisher node parameters')
-    ld.add_action(yaml_path_arg)
 
     remappings = [('robot_state', '/odri/robot_state')]
 
@@ -36,6 +24,8 @@ def generate_launch_description():
                 remappings=remappings,
                 namespace='odri')
 
+    ld.add_action(robot_name_arg)
+    ld.add_action(yaml_path_arg)
     ld.add_action(node)
 
     return ld
